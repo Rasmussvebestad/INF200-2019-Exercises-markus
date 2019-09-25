93ee6012eba36c150f3178544ca8b81deeedc6ab
Markus Ola Granheim <mgranhei@nmbu.no>
2019-09-25 15:01:10 +0200

    task 2b - finishing the code before PEP8

--- src/YOUR_NAME_ex/ex01/message_entropy.py
+++ src/YOUR_NAME_ex/ex01/message_entropy.py
@@ -1,14 +1,28 @@
+def letter_freq(txt):
+    streng = txt.lower()
+    streng = ''.join(sorted(streng))
+    tekst = collections.OrderedDict()
+    for symbol in streng:
+        if symbol in tekst:
+            tekst[symbol] += 1
+        else:
+            tekst[symbol] = 1
+    return tekst
 
-def entropy(message):
-    
+import math as m
+
+def entropy(text = input('Please enter text to analyse: ')):
+
+    tekst = letter_freq('message')
+    dict_keys = list(tekst.values())
+    N = len(text)
+    sum = 0
+
+    for i in range(len(dict_keys)):
+        sum += ((dict_keys[i]/N) * m.log2(dict_keys[i] / N))
+
     pass
-
-
-
-if __name__ == "__main__":
-    for msg in '', 'aaaa', 'aaba', 'abcd', 'This is a short text.':
-        print('{:25}: {:8.3f} bits'.format(msg, entropy(msg)))
-
+    return(sum)
 
 
 
