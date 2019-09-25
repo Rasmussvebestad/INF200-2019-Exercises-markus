efa3f55608f1b065c3c6ed06be860b33d2f93eaf
Markus Ola Granheim <mgranhei@nmbu.no>
2019-09-25 16:42:23 +0200

    fixing pep8

--- src/YOUR_NAME_ex/ex01/message_entropy.py
+++ src/YOUR_NAME_ex/ex01/message_entropy.py
@@ -15,15 +15,18 @@ def letter_freq(txt):
     return tekst
 
 
-def entropy(text=input('Please enter text to analyse: ')):
-    tekst = letter_freq('message')
+def entropy(text):
+    tekst = letter_freq(text)
     dict_keys = list(tekst.values())
     n = len(text)
     totalt = 0
 
     for i in range(len(dict_keys)):
-        totalt += ((dict_keys[i] / n) * m.log2(dict_keys[i] / n))
+        totalt += -((dict_keys[i] / n) * m.log2(dict_keys[i] / n))
 
     return totalt
 
-    pass
+
+if __name__ == "__main__":
+    for msg in '', 'aaaa', 'aaba', 'abcd', 'This is a short text.':
+        print('{:25}: {:8.3f} bits'.format(msg, entropy(msg)))
